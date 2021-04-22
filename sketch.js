acc164446b06e04f9f5237bc3ec96b9fd36752cb
Tanmay176 <79142732+Tanmay176@users.noreply.github.com>
2021-04-22 17:03:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var gamestate=0;
+var playercount;
+var database;
+var form,game,player;
+
+function setup(){
+    createCanvas(500,500);
+    database=firebase.database()
+    game=new Game()
+    game.getState()
+    game.start()    
+}
+
+function draw(){
+    background("white");
+    
+}
